732a084cffccca63f85c6005d12ff7338b18ed7b
Officialvic <onaopeadeoye594@gmail.com>
2022-10-21 10:21:10 -0700

    Variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - base16
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
